fdca78e2b349d08e3909c0286746fb29daaff37d
bobismijnnaam <bobrubbens@gmail.com>
2016-11-21 12:13:57 +0100

    Added a small guide in the readme and cleaned up the input code a bit.

--- src/joystick_input.cpp
+++ src/joystick_input.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <cmath>
+#include <boost/optional.hpp>
 
 #include "joystick_input.h"
 #include "ros/ros.h"
@@ -9,199 +10,130 @@
 
 namespace rtt {
 
-ros::Publisher chatter1_pub;
-ros::Publisher chatter2_pub;
+struct JoystickMap {
+    int speedAxis;
+    int directionAxis;
+    int rotationAxis;
+    int dribblerAxis;
+    int kickerAxis;
+} ;
 
-void Joy::joyCallback1(const sensor_msgs::Joy& msg) {
-    ros::param::get("roboteam_input_1/robot1", id);
-    x_vel= pow(msg.axes[speedaxis], 3) * 3;
-    y_vel= pow(msg.axes[directionaxis], 3) * 3;
-    w = pow(msg.axes[2], 3) * 5;
-	//ROS_INFO(typeid(msg.axes[dribbleraxis]).name());
+const int NUM_CONTROLLERS = 4;
+
+const std::map<std::string, JoystickMap> joystickTypeMap = {
+    {
+        {"xbox", {
+            1, // speedAxis
+            0, // directionAxis
+            3, // rotationAxis
+            4, // dribblerAxis
+            5 // kickerAxis
+        }}, {"playstation", {
+            1, // speedAxis
+            0, // directionAxis
+            2, // rotationAxis
+            10, // dribblerAxis
+            11 // kickerAxis
+        }}
+    }
+};
+
+std::array<boost::optional<sensor_msgs::Joy>, NUM_CONTROLLERS> joyMsgs;
+
+void receiveJoyMsg(int inputNum, const sensor_msgs::JoyConstPtr& msg) {
+    // Only store the newest messages
+    joyMsgs[inputNum] = *msg;   
+}
+
+roboteam_msgs::RobotCommand makeRobotCommand(const int inputNum, const sensor_msgs::Joy& msg) {
+    // Everything is within the roboteam_input node, in groups going from
+    // input0 to inputN.
+    const std::string group = "~input" + std::to_string(inputNum);
+
+    // Get the joystick type
+    std::string joyType = "xbox";
+    ros::param::get(group + "/joyType", joyType);
+    const JoystickMap &joystickMap = joystickTypeMap.at(joyType);
+
+    // Get the robot id
+    int ROBOT_ID = 0;
+    ros::param::get(group + "/robot", ROBOT_ID);
+
+    // Construct the robot command
     roboteam_msgs::RobotCommand command;
-    float dribbler_input = msg.buttons[10];
-    float kicker_input = msg.buttons[11];
-
-    dribbler = dribbler_input > 0;
-    if (kicker_input > 0) {
-        kick_vel = 8.0;
+    command.id = ROBOT_ID;
+    command.active = true;
+    command.x_vel= pow(msg.axes[joystickMap.speedAxis], 3) * 3;
+    command.y_vel= pow(msg.axes[joystickMap.directionAxis], 3) * 3;
+    command.w = pow(msg.axes[joystickMap.rotationAxis], 3) * 5;
+    command.dribbler = msg.buttons[joystickMap.dribblerAxis] > 0;
+    command.kicker = msg.buttons[joystickMap.kickerAxis] > 0;
+    if (command.kicker > 0) {
+        command.kicker_vel = 8.0;
     }
 
-    command.id = id;
-    command.active = active;
-    command.x_vel = x_vel;
-    command.y_vel = y_vel;
-    command.w = w;
-    command.dribbler = dribbler;
-    command.kicker = kicker_input > 0;
-    command.kicker_vel = kick_vel;
-    pub.publish(command);
-}
-
-void Joy::joyCallback2(const sensor_msgs::Joy& msg) {
-	ros::param::get("roboteam_input_1/robot2", id);
-    x_vel= pow(msg.axes[speedaxis], 3) * 3;
-    y_vel= pow(msg.axes[directionaxis], 3) * 3;
-    w = pow(msg.axes[rotationaxis], 3) * 5;
-	//ROS_INFO(typeid(msg.axes[dribbleraxis]).name());
-    roboteam_msgs::RobotCommand command;
-	float dribbler_input = msg.buttons[dribbleraxis];
-    float kicker_input = msg.buttons[kickeraxis];
-
-    dribbler = dribbler_input > 0;
-    if (kicker_input > 0) {
-        kick_vel = 8.0;
-    }
-
-    command.id = id;
-    command.active = active;
-    command.x_vel = x_vel;
-    command.y_vel = y_vel;
-    command.w = w;
-    command.dribbler = dribbler;
-    command.kicker = kicker_input > 0;
-    command.kicker_vel = kick_vel;
-    pub.publish(command);
-}
-
-void Joy::joyCallback3(const sensor_msgs::Joy& msg) {
-	ros::param::get("roboteam_input_1/robot3", id);
-    x_vel= pow(msg.axes[speedaxis], 3) * 3;
-    y_vel= pow(msg.axes[directionaxis], 3) * 3;
-    w = pow(msg.axes[rotationaxis], 3) * 5;
-	//ROS_INFO(typeid(msg.axes[dribbleraxis]).name());
-    roboteam_msgs::RobotCommand command;
-    float dribbler_input = msg.buttons[dribbleraxis];
-    float kicker_input = msg.buttons[kickeraxis];
-
-    dribbler = dribbler_input > 0;
-    if (kicker_input > 0) {
-        kick_vel = 8.0;
-    }
-
-    command.id = id;
-    command.active = active;
-    command.x_vel = x_vel;
-    command.y_vel = y_vel;
-    command.w = w;
-    command.dribbler = dribbler;
-    command.kicker = kicker_input > 0;
-    command.kicker_vel = kick_vel;
-    pub.publish(command);
-}
-
-void Joy::joyCallback4(const sensor_msgs::Joy& msg) {
-	ros::param::get("roboteam_input_1/robot4", id);
-    x_vel= pow(msg.axes[speedaxis], 3) * 3;
-    y_vel= pow(msg.axes[directionaxis], 3) * 3;
-    w = pow(msg.axes[rotationaxis], 3) * 5;
-	//ROS_INFO(typeid(msg.axes[dribbleraxis]).name());
-    roboteam_msgs::RobotCommand command;
-    float dribbler_input = msg.buttons[dribbleraxis];
-    float kicker_input = msg.buttons[kickeraxis];
-
-    dribbler = dribbler_input > 0;
-    if (kicker_input > 0) {
-        kick_vel = 8.0;
-    }
-
-    command.id = id;
-    command.active = active;
-    command.x_vel = x_vel;
-    command.y_vel = y_vel;
-    command.w = w;
-    command.dribbler = dribbler;
-    command.kicker = kicker_input > 0;
-    command.kicker_vel = kick_vel;
-    pub.publish(command);
-}
-
-void Joy::loop() {
-    while (ros::ok()) {
-
-        std::string myJoyNow1;
-        ros::param::get("roboteam_input_1/joy1", myJoyNow1);
-        if (myJoyNow1 != myJoy1) {
-        	if (myJoyNow1 == "none") {
-        		ROS_INFO_STREAM("stopped listening for joystick 1");
-        		sub1.shutdown();
-        		myJoy1 = myJoyNow1;
-        	} else {
-	            ROS_INFO_STREAM("listening to topic " << myJoyNow1 << ", for joystick 1");
-	            sub1.shutdown();
-	            myJoy1 = myJoyNow1;
-	            sub1 = n.subscribe(myJoy1, 10, &Joy::joyCallback1, this);
-	        }
-        }
-
-        std::string myJoyNow2;
-        ros::param::get("roboteam_input_1/joy2", myJoyNow2);
-        if (myJoyNow2 != myJoy2) {
-            if (myJoyNow2 == "none") {
-        		ROS_INFO_STREAM("stopped listening for joystick 2");
-        		sub2.shutdown();
-        		myJoy2 = myJoyNow2;
-        	} else {
-	            ROS_INFO_STREAM("listening to topic " << myJoyNow2 << ", for joystick 2");
-	            sub2.shutdown();
-	            myJoy2 = myJoyNow2;
-	            sub2 = n.subscribe(myJoy2, 10, &Joy::joyCallback2, this);
-	        }
-        }
-
-
-        std::string myJoyNow3;
-        ros::param::get("roboteam_input_1/joy3", myJoyNow3);
-        if (myJoyNow3 != myJoy3) {
-            if (myJoyNow3 == "none") {
-        		ROS_INFO_STREAM("stopped listening for joystick 3");
-        		sub3.shutdown();
-        		myJoy3 = myJoyNow3;
-        	} else {
-	            ROS_INFO_STREAM("listening to topic " << myJoyNow3 << ", for joystick 2");
-	            sub3.shutdown();
-	            myJoy3 = myJoyNow3;
-	            sub3 = n.subscribe(myJoy3, 10, &Joy::joyCallback3, this);
-	        }
-        }
-
-        std::string myJoyNow4;
-        ros::param::get("roboteam_input_1/joy4", myJoyNow4);
-        if (myJoyNow4 != myJoy4) {
-            if (myJoyNow4 == "none") {
-        		ROS_INFO_STREAM("stopped listening for joystick 4");
-        		sub4.shutdown();
-        		myJoy4 = myJoyNow4;
-        	} else {
-	            ROS_INFO_STREAM("listening to topic " << myJoyNow4 << ", for joystick 4");
-	            sub4.shutdown();
-	            myJoy4 = myJoyNow4;
-	            sub4 = n.subscribe(myJoy4, 10, &Joy::joyCallback4, this);
-	        }
-        }
-
-        ros::spinOnce();
-
-    }
-}
-
-Joy::Joy(ros::NodeHandle nh) {
-    n = nh;
-    pub = n.advertise<roboteam_msgs::RobotCommand>("robotcommands", 1000);
-
-    ROS_INFO("startup ok");
-    Joy::loop();
-}
-
-Joy::~Joy() {
-    n.shutdown();
+    return command;
 }
 
 } // rtt
 
 int main(int argc, char **argv) {
+    using namespace rtt;
+
     ros::init(argc, argv, "roboteam_input");
     ros::NodeHandle n;
-    rtt::Joy joy(n);
+
+    // Make sure there are only "none's" in the array
+    joyMsgs.fill(boost::none);
+    
+    // For each input, construct a subscriber and store it in this vector
+    // If we'd want to switch between inpus based on some topic, this vector
+    // would have to be made global, and subscribers would have to be deleted
+    // from the array and inserted again with proper callbacks.
+    // At the same time, the joy nodes would also have to be (re)configured.
+    // (right now this happens in the launch file roboteam_input.launch)
+    // Right now the joy nodes have no "reconfigure" topics or anything, so
+    // we'd have to write our own joystick code that reads from
+    // /dev/input/js0 et al.
+    std::vector<std::unique_ptr<ros::Subscriber>> subscribers;
+    for (int i = 0; i < NUM_CONTROLLERS; ++i) {
+        // Make a subscriber on the stack
+        auto subscriber = n.subscribe<sensor_msgs::Joy>("js" + std::to_string(i), 1, boost::bind(receiveJoyMsg, i, _1));
+        // Construct a copy on the heap
+        std::unique_ptr<ros::Subscriber> subscriberHeap(new ros::Subscriber(subscriber));
+        // Push it on the vector
+        subscribers.push_back(std::move(subscriberHeap));
+    }
+
+    auto pub = n.advertise<roboteam_msgs::RobotCommand>("robotcommands", 1000);
+    
+    // TODO: Right now the roboteam_input's input names (js0, js1) are hardcoded
+    // in the launch file. they should probably also be changeable through an event/topic
+    // However, the joy & joy_node nodes are fixed, so not sure if this is possible
+    // without writing our own joynode code.
+    
+    // Flush received messages every 1/30th second
+    ros::Rate fps30(30);
+
+    while (ros::ok()) {
+        // For every controller...
+        for (int i = 0; i < NUM_CONTROLLERS; ++i) {
+            // If a message is present
+            if (joyMsgs[i]) {
+                // Make a command out of it
+                auto command = makeRobotCommand(i, *joyMsgs[i]);
+                // Send it
+                pub.publish(command);
+
+                // Reset the element in the array
+                joyMsgs[i] = boost::none;
+            }
+        }
+
+        fps30.sleep();
+        
+        ros::spinOnce();
+    }
+
     return 0;
 }
