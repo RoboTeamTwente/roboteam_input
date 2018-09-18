fff8bb954e263a8271296226ca0b6dc9863795ca
mrlukasbos <mrlukasbos@gmail.com>
2018-09-18 14:28:55 +0200

    Improve structure of tests

--- test/KeyboardManagerTest.cpp
+++ test/KeyboardManagerTest.cpp
@@ -1,89 +1,102 @@
+/*
+ * This test checks for all possible keyboard inputs if the robotcommand is correct.
+ */
+
 #include <gtest/gtest.h>
 #include <keyboard/keyboard_manager.h>
 #include <SDL.h>
 
-KeyboardManager keyboard;
-roboteam_msgs::RobotCommand cmd;
-
-void generateKeyPress(Uint32 type, SDL_Keycode key, Uint8 repeat = 0) {
+void simulateKeyPress(KeyboardManager * keyboard, Uint32 type, SDL_Keycode key, Uint8 repeat = 0) {
     SDL_Event test_event;
     test_event.type = type;
-
-    // please note that repeat is an optional parameter
     test_event.key.repeat = repeat;
     test_event.key.keysym.sym = key;
-    keyboard.handleSDLEvents(test_event);
+    keyboard->handleSDLEvents(test_event);
+}
+
+roboteam_msgs::RobotCommand getCommandForKeyPress(KeyboardManager * keyboard, Uint32 type, SDL_Keycode key, Uint8 repeat = 0) {
+    simulateKeyPress(keyboard, type, key, repeat);
+    return keyboard->GetRobotCommand();
 }
 
 // dribbler
 TEST(KeyboardTest, It_toggles_the_dribbler) {
+    KeyboardManager keyboard;
+    roboteam_msgs::RobotCommand cmd;
+
     bool prevDribblerVal = keyboard.GetRobotCommand().dribbler;
 
     // toggle it so it becomes different from before
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_DRIBBLE);
-    EXPECT_FALSE(keyboard.GetRobotCommand().dribbler == prevDribblerVal);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_DRIBBLE);
+    EXPECT_FALSE(cmd.dribbler == prevDribblerVal);
 
     // this keyup event should not let the test fail
-    generateKeyPress(SDL_KEYUP, constants::KEY_DRIBBLE);
-    EXPECT_FALSE(keyboard.GetRobotCommand().dribbler == prevDribblerVal);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYUP, constants::KEY_DRIBBLE);
+    EXPECT_FALSE(cmd.dribbler == prevDribblerVal);
 
     // toggle again so it is equal to prevDribblerVal again
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_DRIBBLE);
-    EXPECT_TRUE(keyboard.GetRobotCommand().dribbler == prevDribblerVal);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_DRIBBLE);
+    EXPECT_TRUE(cmd.dribbler == prevDribblerVal);
 }
 
 TEST(KeyboardTest, It_strafes) {
+    KeyboardManager keyboard;
+    roboteam_msgs::RobotCommand cmd;
+
     // Strafe right
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_STRAFE_RIGHT);
-    ASSERT_LT(keyboard.GetRobotCommand().y_vel, 0);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_STRAFE_RIGHT);
+    ASSERT_LT(cmd.y_vel, 0);
 
     // stop strafing right
-    generateKeyPress(SDL_KEYUP, constants::KEY_STRAFE_RIGHT);
-    ASSERT_EQ(keyboard.GetRobotCommand().y_vel, 0);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYUP, constants::KEY_STRAFE_RIGHT);
+    ASSERT_EQ(cmd.y_vel, 0);
 
     // strafe left
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_STRAFE_LEFT);
-    ASSERT_GT(keyboard.GetRobotCommand().y_vel, 0);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_STRAFE_LEFT);
+    ASSERT_GT(cmd.y_vel, 0);
 
     // stop strafe left
-    generateKeyPress(SDL_KEYUP, constants::KEY_STRAFE_LEFT);
-    ASSERT_EQ(keyboard.GetRobotCommand().y_vel, 0);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYUP, constants::KEY_STRAFE_LEFT);
+    ASSERT_EQ(cmd.y_vel, 0);
 }
 
-// kicker
 TEST(KeyboardTest, It_kicks) {
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_KICK);
-    cmd = keyboard.GetRobotCommand();
+    KeyboardManager keyboard;
+    roboteam_msgs::RobotCommand cmd;
+
+    // do kick on keypress
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_KICK);
     EXPECT_TRUE(cmd.kicker && cmd.kicker_forced);
 
-    generateKeyPress(SDL_KEYUP, constants::KEY_KICK);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYUP, constants::KEY_KICK);
     EXPECT_TRUE(!cmd.kicker && !cmd.kicker_forced);
 }
 
 // chipper
 TEST(KeyboardTest, It_chips) {
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_CHIP);
-    cmd = keyboard.GetRobotCommand();
+    KeyboardManager keyboard;
+    roboteam_msgs::RobotCommand cmd;
+
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_CHIP);
     ASSERT_TRUE(cmd.chipper && cmd.chipper_forced);
 
-    generateKeyPress(SDL_KEYUP, constants::KEY_CHIP);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYUP, constants::KEY_CHIP);
     ASSERT_TRUE(!cmd.chipper && !cmd.chipper_forced);
 }
 
 TEST(KeyboardTest, It_changes_kick_and_chip_force) {
+    KeyboardManager keyboard;
+    roboteam_msgs::RobotCommand cmd;
 
     for (int i = 0; i < 100; i++) {
-        generateKeyPress(SDL_KEYDOWN, constants::KEY_INCREASE_KICK);
+        simulateKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_INCREASE_KICK);
     }
 
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_KICK);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_KICK);
     EXPECT_EQ(cmd.kicker_vel, roboteam_msgs::RobotCommand::MAX_KICKER_VEL);
 
     for (int i = 0; i < 100; i++) {
-        generateKeyPress(SDL_KEYDOWN, constants::KEY_DECREASE_KICK);
+        simulateKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_DECREASE_KICK);
     }
 
     cmd = keyboard.GetRobotCommand();
@@ -92,112 +105,108 @@ TEST(KeyboardTest, It_changes_kick_and_chip_force) {
 
 // Geneva turning test and boundary check
 TEST(KeyboardTest, It_turns_geneva) {
+    KeyboardManager keyboard;
+    roboteam_msgs::RobotCommand cmd;
+
     int InitialGenevaState = keyboard.GetRobotCommand().geneva_state;
 
     // turn left
     for (int i = 1; i <= 5; i++) {
-        generateKeyPress(SDL_KEYUP, constants::KEY_GENEVA_LEFT); // KEYUP must not make a difference
-        generateKeyPress(SDL_KEYDOWN, constants::KEY_GENEVA_LEFT);
-        int genevaState = keyboard.GetRobotCommand().geneva_state;
-        EXPECT_EQ(genevaState, std::max(InitialGenevaState - i, 1));
+        simulateKeyPress(&keyboard, SDL_KEYUP, constants::KEY_GENEVA_LEFT); // KEYUP must not make a difference
+        cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_GENEVA_LEFT);
+        EXPECT_EQ(cmd.geneva_state, std::max(InitialGenevaState - i, 1));
     }
 
     // turn right
     InitialGenevaState = keyboard.GetRobotCommand().geneva_state;
     for (int i = 1; i <= 5; i++) {
-        generateKeyPress(SDL_KEYUP, constants::KEY_GENEVA_RIGHT); // KEYUP must not make a difference
-        generateKeyPress(SDL_KEYDOWN, constants::KEY_GENEVA_RIGHT);
-        int genevaState = keyboard.GetRobotCommand().geneva_state;
-        EXPECT_EQ(genevaState, std::min(InitialGenevaState + i, 5));
+        simulateKeyPress(&keyboard, SDL_KEYUP, constants::KEY_GENEVA_RIGHT); // KEYUP must not make a difference
+        cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_GENEVA_RIGHT);
+        EXPECT_EQ(cmd.geneva_state, std::min(InitialGenevaState + i, 5));
     }
 
     // turn left but with key pressed on repeat, which shouldn't do anything.
-    InitialGenevaState = keyboard.GetRobotCommand().geneva_state;
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_GENEVA_RIGHT, 1);
-    EXPECT_EQ(keyboard.GetRobotCommand().geneva_state, InitialGenevaState);
+    InitialGenevaState = cmd.geneva_state;
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_GENEVA_RIGHT, 1);
+    EXPECT_EQ(cmd.geneva_state, InitialGenevaState);
 
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_GENEVA_RIGHT, 1);
-    EXPECT_EQ(keyboard.GetRobotCommand().geneva_state, InitialGenevaState);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_GENEVA_RIGHT, 1);
+    EXPECT_EQ(cmd.geneva_state, InitialGenevaState);
 
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_GENEVA_LEFT, 1);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_GENEVA_LEFT, 1);
     EXPECT_EQ(keyboard.GetRobotCommand().geneva_state, InitialGenevaState);
 }
 
 
 TEST(KeyboardTest, It_rotates_robot) {
+    KeyboardManager keyboard;
+    roboteam_msgs::RobotCommand cmd;
+
     float oldAngle =  keyboard.GetRobotCommand().w;
-    generateKeyPress(SDL_KEYDOWN, SDLK_LEFT);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, SDLK_LEFT);
     EXPECT_TRUE(cmd.use_angle && cmd.w > oldAngle);
     float angleDifference = cmd.w - oldAngle;
 
     // Test if changing angle speed does something
     oldAngle = keyboard.GetRobotCommand().w;
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_INCREASE_ROTATION_SPEED);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_INCREASE_ROTATION_SPEED);
     float newAngleDifference = cmd.w - oldAngle;
     oldAngle = cmd.w;
 
     EXPECT_GT(newAngleDifference, angleDifference);
 
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_DECREASE_ROTATION_SPEED);
-    generateKeyPress(SDL_KEYDOWN, SDLK_LEFT);
-    cmd = keyboard.GetRobotCommand();
+    simulateKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_DECREASE_ROTATION_SPEED);
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, SDLK_LEFT);
     newAngleDifference = cmd.w - oldAngle;
     EXPECT_NEAR(newAngleDifference, angleDifference, 0.1);
 
     // these are some repeated keypresses
     // Test limits
     for (int i = 0; i < 200; i++) {
-        generateKeyPress(SDL_KEYDOWN, SDLK_LEFT, 1);
-        EXPECT_LT(cmd.w, 32);
-        EXPECT_GT(cmd.w, 0);
+        cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, SDLK_LEFT, 1);
+        EXPECT_LT(cmd.w, 16*M_PI);
+        EXPECT_GT(cmd.w, -16*M_PI);
     }
 
     // stop rotating on keyup
-    oldAngle =  keyboard.GetRobotCommand().w;
-    generateKeyPress(SDL_KEYUP, SDLK_LEFT);
-    cmd = keyboard.GetRobotCommand();
+    oldAngle = keyboard.GetRobotCommand().w;
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYUP, SDLK_LEFT);
     EXPECT_EQ(cmd.w, oldAngle);
 }
 
 TEST(KeyboardTest, It_drives_forward_and_backward) {
+    KeyboardManager keyboard;
+    roboteam_msgs::RobotCommand cmd;
+
     // make sure we are driving (we simulate using xvel, which is up and down.)
-    generateKeyPress(SDL_KEYDOWN, SDLK_UP);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, SDLK_UP);
     EXPECT_GT(cmd.x_vel, 0);
     float velocityDifference = cmd.x_vel;
 
     // stop pressing so the velocity should be 0 again
-    generateKeyPress(SDL_KEYUP, SDLK_UP);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYUP, SDLK_UP);
     EXPECT_EQ(cmd.x_vel, 0);
 
     // increase speed
-    generateKeyPress(SDL_KEYDOWN, constants::KEY_INCREASE_VEL);
+    simulateKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_INCREASE_VEL);
 
     // the speed difference should be higher
-    generateKeyPress(SDL_KEYDOWN, SDLK_UP);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, SDLK_UP);
     EXPECT_GT(cmd.x_vel, velocityDifference);
 
     // test limits
-    cmd.y_vel = 0;
     for (int i = 0; i < 100; i++) {
-        generateKeyPress(SDL_KEYDOWN, constants::KEY_INCREASE_VEL);
+        getCommandForKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_INCREASE_VEL);
     }
 
-    generateKeyPress(SDL_KEYDOWN, SDLK_DOWN);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, SDLK_DOWN);
     EXPECT_NEAR(std::abs(cmd.x_vel), constants::MAX_ROBOT_VELOCITY, 0.5); // TODO make this step size
 
-    cmd.y_vel = 0;
     for (int i = 0; i < 100; i++) {
-        generateKeyPress(SDL_KEYDOWN, constants::KEY_DECREASE_VEL);
+        simulateKeyPress(&keyboard, SDL_KEYDOWN, constants::KEY_DECREASE_VEL);
     }
 
-    generateKeyPress(SDL_KEYDOWN, SDLK_DOWN);
-    cmd = keyboard.GetRobotCommand();
+    cmd = getCommandForKeyPress(&keyboard, SDL_KEYDOWN, SDLK_DOWN);
     EXPECT_EQ(std::abs(cmd.x_vel), constants::MIN_ROBOT_VELOCITY);
 }
 
