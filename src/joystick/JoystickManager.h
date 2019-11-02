5a0f83fc21d0dbbcea26573e1b0b74b4827937dd
barisimre <b.imre@student.utwente.nl>
2019-11-02 17:27:36 +0100

    Made JoystickManager thread-safe

--- src/joystick/JoystickManager.h
+++ src/joystick/JoystickManager.h
@@ -7,10 +7,11 @@
 
 #include <memory>
 #include <map>
-#include "JoystickHandler.h"
+#include <unistd.h>
+#include <mutex>
 #include <SDL.h>
 #include <SDL_joystick.h>
-#include <unistd.h>
+#include "JoystickHandler.h"
 
 namespace rtt {
 namespace input {
@@ -18,16 +19,32 @@ namespace input {
 class JoystickManager {
 public:
     JoystickManager();
+    bool run();
+    void activate();
+    void deactivate();
+    void stop();
 
 private:
     const int TICK_INTERVAL = 20;
     std::map<int, JoystickHandler*> joystickHandlers;
 
-    void init();
-    void loop();
-    void handleJoystickAdded(const SDL_Event& evt);
-    void handleJoystickRemoved(const SDL_Event& evt);
+    // TODO Check if these can be non-static
+    static std::mutex runningLock;
+    static std::mutex activeLock;
+    // Indicates whether the loop should stop
+    bool running = true;
+    // Indicates whether packets should be handled and joystickHandlers ticked
+    bool active = true;
+    std::unique_ptr<roboteam_proto::Publisher> pub;
 
+    bool init();
+    void loop();
+    bool isRunning();
+    bool isActive();
+    void handleJoystickAdded(const SDL_Event& event);
+    void handleJoystickRemoved(const SDL_Event& event);
+    void tickJoystickHandlers();
+    void handleEvent(SDL_Event& event);
 };
 
 }
